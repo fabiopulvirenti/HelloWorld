c749b99aae7e4d28ef710729088cd67b2b7a5cc3
fabiopulvirenti <fpulvirenti002@gmail.com>
2023-08-22 19:49:26 +0100

    created the first task

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class FirstTask {
     public static void main(String[] args) {
-        
+        String username = "Admin";
+        String password = "Password";
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String enteredUsername = reader.nextLine();
+        System.out.print("Enter password: ");
+        String enteredPassword = reader.nextLine();
+//
+        if (username.equals(enteredUsername) && (password.equals(enteredPassword))) {
+            System.out.println("Welcome...");
+        } else {
+            System.out.println("Access Denied");
+        }
     }
 }
